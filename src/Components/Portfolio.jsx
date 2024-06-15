372a8175e6866c4d0699ae36b001644aba976cad
psimon178 <pryor.simon47@gmail.com>
2024-06-15 21:27:32 +0000

    finalchangesforpryor

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -47,7 +47,7 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideInLeft"; }}
+            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideInLeft" }}
             
           />
         </div>
