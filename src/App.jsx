14d90cf101bc7198d9f3a9526f1a8705586be3cc
psimon178 <pryor.simon47@gmail.com>
2024-06-15 19:19:43 +0000

    commitmade

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pryor Simon",
+  title: "Product Manager | Cornell Engineering",
+  email: "pps43@cornell.edu",
+  gitHub: "psimon176",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/pryor-simon/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
